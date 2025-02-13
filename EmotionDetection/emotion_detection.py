4ad1f346cc23ef9a1f9455d7c0a6b6504f0da584
Cuiyc <yongchang.cui@icloud.com>
2025-02-13 07:51:54 -0500

    add 400

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,21 +4,26 @@ import json
 URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
+BAD_RESPONSE = {
+"anger": None, "disgust": None,  "fear": None, "joy": None, "sadness": None, "dominant_emotion": None }
+
 def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = input_json, headers=HEADERS)
-    resp_json = json.loads(response.text)
-    
-    emotion_scores = resp_json['emotionPredictions'][0]['emotion']
-    dominant_emotion = ''
-    dominant_score = 0
-    for index, (key, value) in enumerate(emotion_scores.items()):
-        if value > dominant_score:
-            dominant_emotion = key
-            dominant_score = value
-    emotion_scores['dominant_emotion'] = dominant_emotion
-    return emotion_scores
+    if response.status_code == 400: 
+        return BAD_RESPONSE
+    else:
+        resp_json = json.loads(response.text)
+        emotion_scores = resp_json['emotionPredictions'][0]['emotion']
+        dominant_emotion = ''
+        dominant_score = 0
+        for index, (key, value) in enumerate(emotion_scores.items()):
+            if value > dominant_score:
+                dominant_emotion = key
+                dominant_score = value
+        emotion_scores['dominant_emotion'] = dominant_emotion
+        return emotion_scores
 
 
 if __name__ == '__main__':
-    emotion_detector("I love this new technology.")
+    print(emotion_detector("I love this new technology."))
