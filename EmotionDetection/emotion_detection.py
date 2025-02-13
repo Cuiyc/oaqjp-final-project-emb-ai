d972c0d8b163c1e67553b64fe7f0c509391de9a2
Cuiyc <yongchang.cui@icloud.com>
2025-02-12 21:46:03 -0500

    update

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+def emotion_detector(text_to_analyze):
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, json = input_json, headers=HEADERS)
+    resp_json = json.loads(response.text)
+    
+    emotion_scores = resp_json['emotionPredictions'][0]['emotion']
+    dominant_emotion = ''
+    dominant_score = 0
+    for index, (key, value) in enumerate(emotion_scores.items()):
+        if value > dominant_score:
+            dominant_emotion = key
+            dominant_score = value
+    emotion_scores['dominant_emotion'] = dominant_emotion
+    return emotion_scores
+
+
+if __name__ == '__main__':
+    emotion_detector("I love this new technology.")
